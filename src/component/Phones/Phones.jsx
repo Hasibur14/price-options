938f2f8f7191fbef653878627203e0b3ea07aed4
hasibur14 <hasib141415@gmail.com>
2024-03-19 22:11:39 +0600

    update

--- src/component/Phones/Phones.jsx
+++ src/component/Phones/Phones.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Bar, BarChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Phones = () => {
 
@@ -13,7 +14,7 @@ const Phones = () => {
         axios.get('https://openapi.programming-hero.com/api/phones?search=iphone')
             .then(data => {
                 const phoneData = data.data.data;
-                const phoneWithFakeData = phoneData.map(item => {
+                const phoneWithFakeData = phoneData.map(phone => {
                     const obj = {
                         name: phone.phone_name,
                         price: parseInt(phone.slug.split('-')[1])
@@ -31,8 +32,14 @@ const Phones = () => {
     return (
         <div>
             <h2 className="text-5xl">Phones:{phones.length}</h2>
+            <BarChart width={1400} height={400} data={phones}>
+          <Bar dataKey="price" fill="#8884d8" />
+              <XAxis dataKey={'name'}></XAxis>
+              <YAxis dataKey={'price'}></YAxis>
+              <Tooltip></Tooltip>
+        </BarChart>
         </div>
-    );
+    );   
 };
 
 export default Phones;
